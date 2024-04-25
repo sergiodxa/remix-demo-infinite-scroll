1a6dfb6bdb3788ef2cc3833011b749a48decc11c
Sergio Xalambrí <hello@sergiodxa.com>
2024-04-25 10:58:09 -0500

    Enable Single Fetch and other v3 flags

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,15 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      future: {
+        unstable_singleFetch: true,
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
